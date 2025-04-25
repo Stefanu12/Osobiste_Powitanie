0bc0adcd0736d6348d33be62d6cf22f1d8f1b309
10376712u <@>
2025-04-25 11:59:30 +0200

    awd

--- app/src/main/java/com/example/osobistepowitanie/MainActivity.java
+++ app/src/main/java/com/example/osobistepowitanie/MainActivity.java
@@ -1,24 +1,81 @@
 package com.example.osobistepowitanie;
 
+import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CHANNEL_ID = "kanal";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        EditText editText = findViewById(R.id.editTextImie);
+
+
+
+        Button but1 = findViewById(R.id.buttonPowitanie);
+        but1.setOnClickListener(v -> {
+            String imie = editText.getText().toString().trim();
+            if (imie.isEmpty()) {
+                AlertDialog();
+            } else {
+                AlertDialog2(imie);
+            }
         });
+
+
     }
+    private void AlertDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Błąd");
+        builder.setMessage("Proszę wpisać swoje imię!");
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"OK",Toast.LENGTH_LONG).show();
+
+            }
+        });
+        builder.create().show();
+    }
+    private void AlertDialog2(String imie){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Potwierdzenie");
+        builder.setMessage("Cześć " + imie + " Czy chcesz otrzymać powiadomienie powitalne?");
+
+        builder.setPositiveButton("Tak, poproszę", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"OK",Toast.LENGTH_LONG).show();
+
+            }
+        });
+        builder.setNegativeButton("Nie, dziękuję", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"kliknięto Anuluj",Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.create().show();
+    }
+
 }
